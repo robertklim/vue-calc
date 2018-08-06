1ef1ed1fe879e997a769f3b2b623eca4e0c3790b
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-06 13:34:08 +0200

    Add math operators (/,*,+,-,=)

--- calc/src/components/Calc.vue
+++ calc/src/components/Calc.vue
@@ -4,22 +4,22 @@
     <div class="btn" @click="clear">C</div>
     <div class="btn" @click="sign">+/-</div>
     <div class="btn" @click="percent">%</div>
-    <div class="btn operator">&divide;</div>
+    <div class="btn operator" @click="divide">&divide;</div>
     <div class="btn" @click="append('7')">7</div>
     <div class="btn" @click="append('8')">8</div>
     <div class="btn" @click="append('9')">9</div>
-    <div class="btn operator">&times;</div>
+    <div class="btn operator" @click="times">&times;</div>
     <div class="btn" @click="append('4')">4</div>
     <div class="btn" @click="append('5')">5</div>   
     <div class="btn" @click="append('6')">6</div>   
-    <div class="btn operator">-</div>   
+    <div class="btn operator" @click="minus">-</div>   
     <div class="btn" @click="append('1')">1</div>   
     <div class="btn" @click="append('2')">2</div>   
     <div class="btn" @click="append('3')">3</div>   
-    <div class="btn operator">+</div> 
+    <div class="btn operator" @click="add">+</div> 
     <div class="btn zero" @click="append('0')">0</div> 
     <div class="btn" @click="dot">.</div>
-    <div class="btn operator">=</div>
+    <div class="btn operator" @click="equal">=</div>
   </div>
 </template>
 
@@ -27,7 +27,10 @@
 export default {
   data() {
     return {
+      previous: null,
       current: '',
+      operator: null,
+      operatorClicked: false,
     }
   },
   methods: {
@@ -42,12 +45,42 @@ export default {
       this.current = `${parseFloat(this.current) / 100}`;
     },
     append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
       this.current = `${this.current}${number}`;
     },
     dot() {
       if (this.current.indexOf('.') === -1) {
         this.append('.');
       }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous), 
+        parseFloat(this.current)
+      )}`;
     },
   },
 }
