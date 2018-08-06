66a158dfda615d9c0677e485d4118f47fb9691e2
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-06 13:09:51 +0200

    Add 'append', 'dot' methods

--- calc/src/components/Calc.vue
+++ calc/src/components/Calc.vue
@@ -5,20 +5,20 @@
     <div class="btn" @click="sign">+/-</div>
     <div class="btn" @click="percent">%</div>
     <div class="btn operator">&divide;</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div class="btn" @click="append('7')">7</div>
+    <div class="btn" @click="append('8')">8</div>
+    <div class="btn" @click="append('9')">9</div>
     <div class="btn operator">&times;</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>   
-    <div class="btn">6</div>   
+    <div class="btn" @click="append('4')">4</div>
+    <div class="btn" @click="append('5')">5</div>   
+    <div class="btn" @click="append('6')">6</div>   
     <div class="btn operator">-</div>   
-    <div class="btn">1</div>   
-    <div class="btn">2</div>   
-    <div class="btn">3</div>   
+    <div class="btn" @click="append('1')">1</div>   
+    <div class="btn" @click="append('2')">2</div>   
+    <div class="btn" @click="append('3')">3</div>   
     <div class="btn operator">+</div> 
-    <div class="btn zero">0</div> 
-    <div class="btn">.</div>
+    <div class="btn zero" @click="append('0')">0</div> 
+    <div class="btn" @click="dot">.</div>
     <div class="btn operator">=</div>
   </div>
 </template>
@@ -40,6 +40,14 @@ export default {
     },
     percent() {
       this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.');
+      }
     },
   },
 }
